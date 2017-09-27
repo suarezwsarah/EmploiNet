a9f2edf9d18eeb315c020ccc96b3e99db2a2e79d
BIDA Mohamed Amine <moumouh206@gmail.com>
2017-09-27 15:25:01 +0100

    Top recruteur final stage

--- app/src/main/java/com/mba2dna/apps/EmploiNet/fragment/PostuleFragment.java
+++ app/src/main/java/com/mba2dna/apps/EmploiNet/fragment/PostuleFragment.java
@@ -4,6 +4,7 @@ package com.mba2dna.apps.EmploiNet.fragment;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.mba2dna.apps.EmploiNet.R;
+import com.mba2dna.apps.EmploiNet.data.Constant;
+import com.mba2dna.apps.EmploiNet.data.SQLiteHandler;
 import com.mba2dna.apps.EmploiNet.model.Offre;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +31,7 @@ public class PostuleFragment extends DialogFragment {
     private Offre mOffre;
     private Button cancel;
     private TextView title;
-
+    private SQLiteHandler db;
     public static PostuleFragment newInstance(Offre mOffre) {
         PostuleFragment fragment = new PostuleFragment();
         fragment.mOffre = mOffre;
@@ -38,6 +48,7 @@ public class PostuleFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_postule, container, false);
+        db = new SQLiteHandler(getContext());
         title = (TextView) v.findViewById(R.id.titleShop);
         title.setText(mOffre.title+" pour le recruteur "+mOffre.contact_info);
         cancel = (Button) v.findViewById(R.id.btncancel);
@@ -47,6 +58,29 @@ public class PostuleFragment extends DialogFragment {
                 dismiss();
             }
         });
+        if(db.isUserExist()){
+            int userId=db.getUserID();
+            OkHttpClient client = new OkHttpClient();
+            String URL = Constant.getURLApiClientData();
+            HttpUrl.Builder urlBuilder = HttpUrl.parse(URL).newBuilder();
+            urlBuilder.addQueryParameter("checkuser", "true");
+            urlBuilder.addQueryParameter("userid", userId+"");
+            String url = urlBuilder.build().toString();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                String S =response.body().string();
+                Log.e("RESPENSE", userId+"");
+                Log.e("RESPENSE", S);
+
+            } catch (IOException e) {
+                Log.e("ERROR",e.getMessage());
+                e.printStackTrace();
+            }
+        }
         setCancelable(false);
         return v;
     }
